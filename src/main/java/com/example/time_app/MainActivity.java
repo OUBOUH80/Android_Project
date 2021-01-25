3465c47f0e38ec1154c5a69d2336eb759fe4e6db
Hassna80 <hasnaaoub70@gmail.com>
2021-01-25 15:32:39 +0100

    XML

---
+++ src/main/java/com/example/time_app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.time_app;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void imgClick(View v){
+        Toast.makeText(this,"Hello",Toast.LENGTH_SHORT).show();
+    }
+}
