7c326e29c22b4cdcd5d278a3195dbfbf6906d340
rajaelm <58122982+rajaelm@users.noreply.github.com>
2021-01-28 23:14:13 +0100

    my activities and added text zone

---
+++ app/src/main/java/inpt/ma/time_app/MesActivites.java
@@ -0,0 +1,14 @@
+package inpt.ma.time_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MesActivites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mes_activites);
+    }
+}
